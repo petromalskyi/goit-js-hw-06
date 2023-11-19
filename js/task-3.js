42f4bfc8ce5a6c3f6e359ac0a7df2427911b69a3
petromalskyi <113782385+petromalskyi@users.noreply.github.com>
2023-11-19 13:09:21 +0200

    finished hw-06

---
+++ js/task-3.js
@@ -0,0 +1,28 @@
+class StringBuilder {
+  value;
+  constructor(str) {
+    this.value = str;
+  }
+  getValue(str) {
+    return this.value;
+  }
+  padEnd(str) {
+    this.value += str;
+  }
+  padStart(str) {
+    this.value = str + this.value;
+  }
+  padBoth(str) {
+    this.padEnd(str);
+    this.padStart(str);
+  }
+}
+
+const builder = new StringBuilder('.');
+console.log(builder.getValue()); // "."
+builder.padStart('^');
+console.log(builder.getValue()); // "^."
+builder.padEnd('^');
+console.log(builder.getValue()); // "^.^"
+builder.padBoth('=');
+console.log(builder.getValue()); // "=^.^="
